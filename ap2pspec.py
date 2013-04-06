e28b4cc7e4b687a44c433c2179271edb72ff7f55
marcin- <marcin.bojara@gmail.com>
2013-04-06 16:55:46 +0200

    update

--- ap2pspec.py
+++ ap2pspec.py
@@ -7,16 +7,35 @@ import sys
 import os
 import re
 
-bgntag = re.compile(".*?<([^!\/\s>]+)[\s>].*")
-endtag = re.compile(".*?<\/(\w+?)>.*")
+bgntag = re.compile("^\s*?<([^!\/\s>]+)[\s>].*")
+endtag = re.compile(".*?<\/(\w+?)>\s*$")
+bgnnote = re.compile("^\s*<!--\s*")
+endnote = re.compile("\s*-->\s*$")
+bgndisabledpatch = re.compile("^\s*<!--\s*<?\s*Patch(>|\s*level)")
+enddisabledpatch = re.compile("<\/Patch>?\s*-->\s*$")
 level = re.compile(".*?level\s?=\s?[\'\"](\d+)[\'\"].*")
 pname = re.compile(".*?>([^<]+)<.*")
 workdir = re.compile("^\s*WorkDir\s*=\s*[\'\"]([^\'^\"]+)[\'\"].*?")
 
 class Patch:
-    def __init__(self, level, patch_file):
+    def __init__(self, level, patch_file, enabled=True):
         self.level = level
         self.name = patch_file
+        self.enabled = enabled
+
+class PatchNote:
+    def __init__(self, index, note):
+        self.index = index
+        self.note = note
+
+def get_notes_dict(notes):
+    notes_dict = {}
+    for n in notes:
+        try:
+            notes_dict[n.index].append(n.note)
+        except KeyError:
+            notes_dict[n.index] = [n.note]
+    return notes_dict
 
 def bgn_tag(data):
     if re.search(bgntag, data):
@@ -62,14 +81,36 @@ def get_pname(line):
 
 def cut_patches(speclines):
     new_spec = []
+    found_notes = []
     found_patches = []
     patches = False
+    disabled_patches = False
+    comment = False
     for line in speclines:
         bt = bgn_tag(line)
         if bt == "Patches":
             patches = True
             continue
         if patches:
+            enabled = True
+            if re.search(bgndisabledpatch, line):
+                disabled_patches = True
+                line = re.sub(bgndisabledpatch, r"<Patch\1", line)
+                bt = bgn_tag(line)
+            elif re.search(bgnnote, line):
+                comment = True
+                line = re.sub(bgnnote, "", line)
+            if disabled_patches: enabled = False
+            if comment:
+                if re.search(endnote, line):
+                    comment = False
+                    line = re.sub(endnote, "", line)
+                line = re.sub("^\s*", "", line)
+                found_notes.append(PatchNote(len(found_patches), line))
+                continue
+            if re.search(enddisabledpatch, line):
+                disabled_patches = False
+                line = re.sub(enddisabledpatch, "</Patch>", line)
             et = end_tag(line)
             if et == "Patches":
                 patches = False
@@ -77,20 +118,38 @@ def cut_patches(speclines):
             if bt == "Patch":
                 l = get_level(line) 
                 n = get_pname(line)
-                if n: found_patches.append(Patch(l, n))
+                if n: found_patches.append(Patch(l, n, enabled))
                 continue
+            if not line or line.isspace(): continue
         new_spec.append(line)
-    return new_spec, found_patches
+    return new_spec, found_patches, found_notes
 
-def insert_patches(speclines, patches):
+def insert_patches(speclines, patches, notes):
+    dict_notes = get_notes_dict(notes)
     new_spec = []
     for line in speclines:
         et = end_tag(line)
         if et == "Source":
             new_spec.append("        <Patches>")
+            cp = 0
             for p in patches:
-                if p.level: new_spec.append('            <Patch level="%s">%s</Patch>' % (p.level, p.name))
-                else: new_spec.append('            <Patch>%s</Patch>' % p.name)
+                if cp in dict_notes:
+                    cn = 0
+                    ln = len(dict_notes[cp])
+                    nline = ""
+                    for n in dict_notes[cp]:
+                        if cn == 0: nline = "            <!-- "
+                        else: nline = "     "
+                        nline = nline + n
+                        if cn + 1 == ln: nline = nline + " -->" 
+                        new_spec.append(nline)
+                if p.level:
+                    if p.enabled: new_spec.append('            <Patch level="%s">%s</Patch>' % (p.level, p.name))
+                    else: new_spec.append('            <!--Patch level="%s">%s</Patch-->' % (p.level, p.name))
+                else:
+                    if p.enabled: new_spec.append('            <Patch>%s</Patch>' % p.name)
+                    else: new_spec.append('            <!--Patch>%s</Patch-->' % p.name)
+                cp += 1
             new_spec.append("        </Patches>")
         new_spec.append(line)
     return new_spec
@@ -110,6 +169,18 @@ def check_level(workdir, path):
         level += 1
         path = path[path.find("/")+1:]
 
+def get_all_patch_files(path):
+    path = path.split("/files")[0]
+    path = "%s/files" % path
+    patch_files_in_root = []
+    patch_files_in_subdirs = []
+    for root, dirs, files in os.walk(path):
+        for f in files:
+            if f.endswith("patch") or f.endswith("diff"):
+                if root == path: patch_files_in_root.append(f)
+                else: patch_files_in_subdirs.append("%s/%s" % (root.replace(path, "")[1:], f))
+    return sorted(patch_files_in_root) + sorted(patch_files_in_subdirs)
+
 if __name__ == '__main__':
     pisi_cmd = "pisi" if os.getenv("USER") == "root" else "sudo pisi"
     specpath = os.getcwd()
@@ -117,6 +188,8 @@ if __name__ == '__main__':
     usage = "Usage: %prog [options] [PATH]"
     parser = OptionParser(usage)
     parser.add_option("-a", "--append", dest="append", help="appends new patche(s)")
+    parser.add_option("-l", "--list-patches", action="store_true", dest="list", help="list pspec patches")
+    parser.add_option("-n", "--list-new-patches", action="store_true", dest="listnew", help="list new patches")
     (options,args) = parser.parse_args()
     try:
         specpath = args[0]
@@ -134,14 +207,24 @@ if __name__ == '__main__':
         print "%s not found!" % specfile
         sys.exit(1)
 
-    spec, patches = cut_patches(read_spec(specfile))
+    spec, patches, patch_notes = cut_patches(read_spec(specfile))
     workdir = get_workdir(actionfile)
-    
-    if options.append:
-        path_glob = glob.glob(path + "/files/%s" % (options.append if not options.append.startswith("files/") else options.append[6:]))
+          
+    def get_old_patches(patches):
         old_patches = []
         for p in patches:
             old_patches.append(p.name)
+        return old_patches
+
+    def get_new_patches(patches, path):
+        new_patches = []
+        old_patches = get_old_patches(patches)
+        for p in get_all_patch_files(path):
+            if not p in old_patches:
+                new_patches.append(p)
+        return new_patches
+
+    if options.append:
         os.system("%s bi %s --unpack" % (pisi_cmd, specfile))
         dirlist = filter(lambda x: os.path.isdir("/var/pisi/" + x), os.listdir("/var/pisi"))
         newest = "/var/pisi/" + max(dirlist, key=lambda x: os.stat("/var/pisi/" + x).st_mtime) + "/work"
@@ -149,11 +232,34 @@ if __name__ == '__main__':
         if workdir: workdir = os.path.normpath("%s/%s" % (newest, workdir))
         else: workdir = "%s/%s" % (newest, os.walk(newest).next()[1][0])
 
+        path_glob = glob.glob(path + "/files/%s" % (options.append if not options.append.startswith("files/") else options.append[6:]))
+        old_patches = get_old_patches(patches)
         for fp in path_glob:
             p = fp.split("/files/")[1]
             if not p in old_patches: 
                 patches.append(Patch(check_level(workdir, get_dest(fp)), p))
         
-        spec = insert_patches(spec, patches)
+        spec = insert_patches(spec, patches, patch_notes)
     
         write_file(specfile, "\n".join(spec))
+
+    if options.list:
+        print "--- current patches (used in pspec.xml) ---"
+        c = 0
+        notes_dict = get_notes_dict(patch_notes)
+        for p in patches:
+            if c in notes_dict:
+                for n in notes_dict[c]:
+                    print "# %s" % n
+            c += 1
+            print ("%d\t%s %s %s" % (c, ("E" if p.enabled else "D"), (p.level if p.level else "0"), p.name))
+        if c in notes_dict:
+            for n in notes_dict[c]:
+                print "# %s" % n
+
+    if options.listnew:
+        print "--- new patches (not used in pspec.xml) ---"
+        c = 0
+        for p in get_new_patches(patches, path):
+            c += 1
+            print "%d\t%s" % (c, p)
