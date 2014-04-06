e9efe9630038db998dd83948b55a4cfdcbc1b0ad
marcin- <marcin.bojara@gmail.com>
2014-04-06 19:45:43 +0200

    skip empty lines

--- cosmetic_fixes.py
+++ cosmetic_fixes.py
@@ -151,6 +151,7 @@ if __name__ == '__main__':
                             params[-1] = params[-1][:-1]
                         new_file.append(firstline + " \\")
                         for p in sorted([" " + i if "/" in i else i for i in params]):
+                            if not p or p.isspace(): continue
                             new_file.append("%s%s \\" % (" " * (len(firstline) + (1 if not p.startswith(" ") else 0)), p))
                         new_file.append(" " * len(firstline) + lastline)
                 else: new_file.append(line)
