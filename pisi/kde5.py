6927922e5970c22b2256d0a374a63ad5b751b45c
marcin- <marcin.bojara@gmail.com>
2015-05-29 17:54:35 +0200

    kde5 for pisi.actionsapi

---
+++ pisi/kde5.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015 marcin.bojara@gmail.com
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING file.
+
+# ActionsAPI Modules
+from pisi.actionsapi import get
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import shelltools
+
+def configure(parameters = "", installPrefix = "/%s" % get.defaultprefixDIR(), buildType = "Release", pythonPath = "/usr/bin/python3", sourceDir = ".."):
+    ''' parameters -DLIB_INSTALL_DIR="target_dir" -DSOMETHING_USEFUL=1'''
+
+    config_params = {
+                     "-DCMAKE_BUILD_TYPE": buildType,
+                     "-DLIB_INSTALL_DIR": "lib",
+                     "-DLIBEXEC_INSTALL_DIR": "libexec",
+                     "-DSYSCONF_INSTALL_DIR": "/etc",
+                     "-DPYTHON_EXECUTABLE": pythonPath,
+                     "-DPLUGIN_INSTALL_DIR": "/usr/lib/qt5/plugins",
+                     "-DECM_MKSPECS_INSTALL_DIR": "/usr/lib/qt5/mkspecs/modules",
+                     "-DKDE_INSTALL_USE_QT_SYS_PATHS": "ON",
+                     "-DBUILD_TESTING": "OFF"
+                     }
+
+    shelltools.makedirs("build")
+    shelltools.cd("build")
+
+    cmaketools.configure("%s %s" % ("".join(['%s="%s"' % (k, v) for k, v in config_params.items() if not "%s=" % k in parameters]), parameters),
+                         installPrefix,
+                         sourceDir)
+
+    shelltools.cd("..")
+
+def make(parameters = ""):
+    cmaketools.make("-C build %s" % parameters)
+
+def install(parameters = "", argument = "install"):
+    cmaketools.install("-C build %s" % parameters, argument)
