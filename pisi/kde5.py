e523a4090ca392690a2665b71ae27500515b99b6
marcin- <marcin.bojara@gmail.com>
2015-05-31 17:45:26 +0200

    keep params order

--- pisi/kde5.py
+++ pisi/kde5.py
@@ -17,24 +17,24 @@ from pisi.actionsapi import shelltools
 def configure(parameters = "", installPrefix = "/%s" % get.defaultprefixDIR(), sourceDir = ".."):
     ''' parameters -DLIB_INSTALL_DIR="target_dir" -DSOMETHING_USEFUL=1'''
 
-    config_params = {
-                     "-DCMAKE_BUILD_TYPE": "Release",
-                     "-DLIB_INSTALL_DIR": "lib",
-                     "-DLIBEXEC_INSTALL_DIR": "libexec",
-                     "-DSYSCONF_INSTALL_DIR": "/etc",
-                     "-DPYTHON_EXECUTABLE": "/usr/bin/python3",
-                     "-DLOCALE_INSTALL_DIR": "/usr/share/locale",
-                     "-DQML_INSTALL_DIR": "lib/qt5/qml",
-                     "-DPLUGIN_INSTALL_DIR": "/usr/lib/qt5/plugins",
-                     "-DECM_MKSPECS_INSTALL_DIR": "/usr/lib/qt5/mkspecs/modules",
-                     "-DKDE_INSTALL_USE_QT_SYS_PATHS": "ON",
-                     "-DBUILD_TESTING": "OFF"
-                     }
+    config_params = [
+                     ("-DSYSCONF_INSTALL_DIR", "/etc"),
+                     ("-DLIB_INSTALL_DIR", "lib"),
+                     ("-DLIBEXEC_INSTALL_DIR", "libexec"),
+                     ("-DLOCALE_INSTALL_DIR", "/usr/share/locale"),
+                     ("-DKDE_INSTALL_USE_QT_SYS_PATHS", "ON"),
+                     ("-DQML_INSTALL_DIR", "lib/qt5/qml"),
+                     ("-DPLUGIN_INSTALL_DIR", "/usr/lib/qt5/plugins"),
+                     ("-DECM_MKSPECS_INSTALL_DIR", "/usr/lib/qt5/mkspecs/modules"),
+                     ("-DBUILD_TESTING", "OFF")
+                     ("-DCMAKE_BUILD_TYPE", "Release"),
+                     ("-DPYTHON_EXECUTABLE", "/usr/bin/python3"),
+                     ]
 
     shelltools.makedirs("build")
     shelltools.cd("build")
 
-    cmaketools.configure("%s %s" % ("".join(['%s="%s" ' % (k, v) for k, v in config_params.items() if not "%s=" % k in parameters]), parameters),
+    cmaketools.configure("%s %s" % ("".join(['%s="%s" ' % (k, v) for k, v in config_params if not "%s=" % k in parameters]), parameters),
                          installPrefix,
                          sourceDir)
 
